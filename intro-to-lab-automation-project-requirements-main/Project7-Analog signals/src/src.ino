57e7cae415c118a0733ba77265fb6be6c41137e6
BC <BC@example.com>
2025-02-04 11:17:48 +0200

    עובד על דימר עם ביפימם

--- intro-to-lab-automation-project-requirements-main/Project7-Analog signals/src/src.ino
+++ intro-to-lab-automation-project-requirements-main/Project7-Analog signals/src/src.ino
@@ -1,21 +1,31 @@
-const int rotaryPin = A0; // Rotary button connected to analog pin A0
-const int OUTPUT_PIN = 4; // LED connected to pin 4
+#define OUTPUT_LED_4 4     // LED connected to pin 4
+
+#include <Arduino.h>
+#include <MD_PWM.h>        // Include the MD_PWM library
+
+// Initialize PWM object
+MD_PWM PWM(4); // PWM on pin 4
 
 void setup() {
-    pinMode(OUTPUT_PIN, OUTPUT); // Set the LED pin as output
-    pinMode(rotaryPin, INPUT); // Set the rotary button pin as input
+    pinMode(OUTPUT_LED_4, OUTPUT); // Set the LED pin as output
+    // Initialize PWM on pin 4
+    PWM.begin();
 }
 
 void loop() {
-    int sensorValue = analogRead(rotaryPin); // Read the value from the rotary button
-    
-    // turn HIGH the LED if the value is greater than 512, otherwise turn it LOW
-    digitalWrite(OUTPUT_PIN, HIGH);
-    digitalWrite(OUTPUT_PIN, LOW);
-    // read the value from the rotary button again
-    analogRead(rotaryPin);
-
-    // turn HIGH the LED if the value is greater than 512, otherwise turn it LOW
-    digitalWrite(OUTPUT_PIN, HIGH);
-    digitalWrite(OUTPUT_PIN, LOW);
+    for (int i = 0; i <= 255; i++) {
+        PWM.write(i);
+        delay(10);
+    }
+    // 3 BLINKS ON 255
+    for (int i = 0; i < 3; i++) {
+        PWM.write(255);
+        delay(500);
+        PWM.write(0);
+        delay(500);
+    }
+    for (int i = 255; i >= 0; i--) {
+        PWM.write(i);
+        delay(10);
+    }
 }
